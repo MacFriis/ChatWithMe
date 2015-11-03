6016f28eac353e6426957b219e268ea46b60290e
Per Friis <pfriis@pers-mbp-2.prv.dk.ibm.com>
2015-11-03 08:12:52 +0100

    Initial Commit

---
+++ Chat With Me/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat With Me
+//
+//  Created by Per Friis on 03/11/15.
+//  Copyright © 2015 Friis Consult aps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
